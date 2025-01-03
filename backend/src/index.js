48169694a12aba57fe12e4f863f9342459e30965
Ahmedbaya <ahdbaya523@gmail.com>
2025-01-03 16:32:26 +0100

    fixes for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,21 +20,22 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? "https://app-chat-r6ed.onrender.com" : "http://localhost:5173",
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));  // Going up 2 directories
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
